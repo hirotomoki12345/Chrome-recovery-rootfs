270fe7f7c6b0a99f9da7664eab00b486fe858d46
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2024-08-29 14:17:12 +0900

    Update recomod.sh

--- recomod.sh
+++ recomod.sh
@@ -268,15 +268,14 @@ EOF
 }
 patch_root_minimal() {
   lsblk
-  tar -cvf rootfs.tar -C "$ROOT" .  
+  tar -cvf rootfs.tar -C "$ROOT" .
   if [ ! -f shimboot_octopus.zip ]; then
     wget -O shimboot_octopus.zip https://dl.darkn.bio/api/raw?path=/Shimboot/shimboot_octopus.zip
   fi
-  
   unzip shimboot_octopus.zip
   SHIMBOOT_LOOPBACK_DEVICE=$(sudo losetup -fP --show shimboot_octopus.bin)
-  sudo dd if=rootfs.tar of=$SHIMBOOT_LOOPBACK_DEVICE bs=4M status=progress
-  echo "rootfs.tar が $SHIMBOOT_LOOPBACK_DEVICE に正常に書き込まれました。"
+  sudo dd if=rootfs.tar of=${SHIMBOOT_LOOPBACK_DEVICE}p4 bs=4M status=progress
+  echo "rootfs.tar が ${SHIMBOOT_LOOPBACK_DEVICE}p4 に正常に書き込まれました。"
 }
 strip_root() {
   # we don't usually need to install chrome, stripping can get the file size down
